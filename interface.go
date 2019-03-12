0903019831bc049fd6b5e0f640bb1aa4b946a890
TruthHun <TruthHun@QQ.COM>
2019-03-12 22:29:19 +0800

    完成又拍云接口实现

--- interface.go
+++ interface.go
@@ -1,9 +1,30 @@
 package CloudStore
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"time"
+)
+
+type File struct {
+	ModTime time.Time
+	Name    string
+	Size    int64
+	IsDir   bool
+	Header  map[string]string
+}
+
 type CloudStore interface {
-	Put(string, string, ...map[string]string) error // 上传文件
-	Delete(...string) error                         // 删除文件
-	GetSignURL(string, int64) (string, error)       // 文件访问签名
-	IsExist(string) error                           // 判断文件是否存在
-	//Lists(prefix stiring)                         // 文件前缀，列出文件
+	Save(string, string, ...map[string]string) error // 上传文件
+	Delete(...string) error                          // 删除文件
+	GetSignURL(string, int64) (string, error)        // 文件访问签名
+	IsExist(string) error                            // 判断文件是否存在
+	Lists(prefix string) ([]File, error)             // 文件前缀，列出文件
+}
+
+// MD5 Crypt
+func MD5Crypt(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
--- upyun.go
+++ upyun.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
+	"github.com/pkg/errors"
 	"github.com/upyun/go-sdk/upyun"
 )
 
@@ -14,9 +16,10 @@ type UpYun struct {
 	Password string
 	Domain   string
 	Client   *upyun.UpYun
+	secret   string
 }
 
-func NewUpYun(bucket, operator, password, domain string) *UpYun {
+func NewUpYun(bucket, operator, password, domain, secret string) *UpYun {
 	if !strings.HasPrefix(domain, "http://") && !strings.HasPrefix(domain, "https://") {
 		domain = "http://" + domain
 	}
@@ -32,44 +35,79 @@ func NewUpYun(bucket, operator, password, domain string) *UpYun {
 		Password: password,
 		Domain:   domain,
 		Client:   client,
+		secret:   secret,
 	}
 }
 
 func (u *UpYun) IsExist(object string) (err error) {
-
+	path := "/" + strings.TrimLeft(object, "./")
+	_, err = u.Client.GetInfo(path)
 	return
 }
 
-func (u *UpYun) Put(tmpFile, saveFile string, header ...map[string]string) (err error) {
+func (u *UpYun) Save(tmpFile, saveFile string, headers ...map[string]string) (err error) {
 	_, err = os.Stat(tmpFile)
-	// TODO: 设置header
-	headers := make(map[string]string)
+	h := make(map[string]string)
 	if err != nil {
 		return
+	}
+	for _, header := range headers {
+		for k, v := range header {
+			h[k] = v
+		}
 	}
 	err = u.Client.Put(&upyun.PutObjectConfig{
 		Path:      saveFile,
 		LocalPath: tmpFile,
-		Headers:   headers,
+		Headers:   h,
 	})
 	return
 }
 
 func (u *UpYun) Delete(object ...string) (err error) {
-	return
-}
-
-func (u *UpYun) GetSignURL(object string, expire int64) (link string, err error) {
-	return
-}
-
-func (u *UpYun) Lists() {
-	objsChan := make(chan *upyun.FileInfo, 10)
-	u.Client.List(&upyun.GetObjectsConfig{
-		Path:        "/",
-		ObjectsChan: objsChan,
-	})
-	for obj := range objsChan {
-		fmt.Printf("%+v\n", obj)
+	var errs []string
+	for _, item := range object {
+		err = u.Client.Delete(&upyun.DeleteObjectConfig{
+			Path: "/" + strings.TrimLeft(item, "./"),
+		})
+		if err != nil {
+			errs = append(errs, err.Error())
+		}
 	}
+	if len(errs) > 0 {
+		err = errors.New(strings.Join(errs, "; "))
+	}
+	return
+}
+
+// https://help.upyun.com/knowledge-base/cdn-token-limite/
+func (u *UpYun) GetSignURL(object string, expire int64) (link string, err error) {
+	path := "/" + strings.TrimLeft(object, "./")
+	if expire <= 0 {
+		return u.Domain + path, nil
+	}
+	endTime := time.Now().Unix() + expire
+	sign := MD5Crypt(fmt.Sprintf("%v&%v&%v", u.secret, endTime, path))
+	sign = strings.Join(strings.Split(sign, "")[12:20], "") + fmt.Sprint(endTime)
+	return u.Domain + path + "?_upt=" + sign, nil
+}
+
+func (u *UpYun) Lists(prefix string) (files []File, err error) {
+	chans := make(chan *upyun.FileInfo, 1000)
+	u.Client.List(&upyun.GetObjectsConfig{
+		Path:        prefix,
+		ObjectsChan: chans,
+	})
+	var file File
+	for obj := range chans {
+		file = File{
+			ModTime: obj.Time,
+			Size:    obj.Size,
+			IsDir:   obj.IsDir,
+			Header:  obj.Meta, // 注意：这里获取不到文件的header
+			Name:    obj.Name,
+		}
+		files = append(files, file)
+	}
+	return
 }
