0b3ccf512381e7c838be8795c37e61e3dfbbeb68
TruthHun <TruthHun@QQ.COM>
2019-03-17 16:43:00 +0800

    七牛云存储

--- qiniu.go
+++ qiniu.go
@@ -2,19 +2,28 @@ package CloudStore
 
 import (
 	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"strings"
+	"time"
 
+	"github.com/astaxie/beego/httplib"
 	"github.com/qiniu/api.v7/auth/qbox"
 	"github.com/qiniu/api.v7/storage"
 )
 
 type QINIU struct {
-	AccessKey string
-	SecretKey string
-	Bucket    string
-	Domain    string
-	Zone      *storage.Zone
-	mac       *qbox.Mac
+	AccessKey     string
+	SecretKey     string
+	Bucket        string
+	Domain        string
+	Zone          *storage.Zone
+	mac           *qbox.Mac
+	BucketManager *storage.BucketManager
 }
 
 func NewQINIU(accessKey, secretKey, bucket, domain string) (q *QINIU, err error) {
@@ -27,13 +36,19 @@ func NewQINIU(accessKey, secretKey, bucket, domain string) (q *QINIU, err error)
 	q.Domain = strings.TrimRight(q.Domain, "/")
 	q.mac = qbox.NewMac(accessKey, secretKey)
 	q.Zone, err = storage.GetZone(accessKey, bucket)
+	if err != nil {
+		return
+	}
+	q.BucketManager = storage.NewBucketManager(q.mac, &storage.Config{Zone: q.Zone})
 	return
 }
 
 func (q *QINIU) IsExist(object string) (err error) {
+	_, err = q.GetInfo(object)
 	return
 }
 
+// TODO: 目前没发现有可以设置header的地方
 func (q *QINIU) Upload(tmpFile, saveFile string, headers ...map[string]string) (err error) {
 	policy := storage.PutPolicy{Scope: q.Bucket}
 	token := policy.UploadToken(q.mac)
@@ -51,79 +66,135 @@ func (q *QINIU) Upload(tmpFile, saveFile string, headers ...map[string]string) (
 	extra := &storage.PutExtra{
 		Params: params,
 	}
-	err = form.PutFile(context.Background(), ret, token, objectRel(saveFile), tmpFile, extra)
+	saveFile = objectRel(saveFile)
+	// 需要先删除，文件存在的话，没法覆盖
+	q.Delete(saveFile)
+	err = form.PutFile(context.Background(), ret, token, saveFile, tmpFile, extra)
 	return
 }
 
-func (q *QINIU) Delete(object ...string) (err error) {
+func (q *QINIU) Delete(objects ...string) (err error) {
+	length := len(objects)
+	if length == 0 {
+		return
+	}
+
+	defer func() {
+		// 被删除文件不存在的时候，err值为空但不为nil，这里处理一下
+		if err != nil && err.Error() == "" {
+			err = nil
+		}
+	}()
+
+	deleteOps := make([]string, 0, length)
+	for _, object := range objects {
+		deleteOps = append(deleteOps, storage.URIDelete(q.Bucket, objectRel(object)))
+	}
+	cfg := &storage.Config{
+		Zone: q.Zone,
+	}
+	manager := storage.NewBucketManager(q.mac, cfg)
+	var res []storage.BatchOpRet
+	res, err = manager.Batch(deleteOps)
+	if err != nil {
+		return
+	}
+
+	var errs []string
+	for _, item := range res {
+		if item.Code != http.StatusOK {
+			errs = append(errs, fmt.Errorf("%+v: %v", item.Data, item.Code).Error())
+		}
+	}
+
+	if len(errs) > 0 {
+		err = errors.New(strings.Join(errs, "; "))
+	}
+
 	return
 }
 
 func (q *QINIU) GetSignURL(object string, expire int64) (link string, err error) {
+	object = objectRel(object)
+	if expire > 0 {
+		deadline := time.Now().Add(time.Second * time.Duration(expire)).Unix()
+		link = storage.MakePrivateURL(q.mac, q.Domain, object, deadline)
+	} else {
+		link = storage.MakePublicURL(q.Domain, object)
+	}
 	return
 }
 
 func (q *QINIU) Download(object string, savePath string) (err error) {
+	var link string
+	link, err = q.GetSignURL(object, 3600)
+	if err != nil {
+		return
+	}
+	req := httplib.Get(link).SetTimeout(30*time.Minute, 30*time.Minute)
+	if strings.HasPrefix(strings.ToLower(link), "https://") {
+		req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	}
+
+	var resp *http.Response
+
+	resp, err = req.Response()
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	data, _ := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusMultipleChoices {
+		return fmt.Errorf("%v: %v", resp.Status, string(data))
+	}
+
+	err = ioutil.WriteFile(savePath, data, os.ModePerm)
+
 	return
 }
 
 func (q *QINIU) GetInfo(object string) (info File, err error) {
+	var fileInfo storage.FileInfo
 
+	object = objectRel(object)
+	fileInfo, err = q.BucketManager.Stat(q.Bucket, object)
+	if err != nil {
+		return
+	}
+	info = File{
+		Name:    object,
+		Size:    fileInfo.Fsize,
+		ModTime: storage.ParsePutTime(fileInfo.PutTime),
+		IsDir:   fileInfo.Fsize == 0,
+	}
 	return
 }
 
 func (q *QINIU) Lists(prefix string) (files []File, err error) {
-	return
-}
+	var items []storage.ListItem
 
-//////////
+	prefix = objectRel(prefix)
+	limit := 1000
+	cfg := &storage.Config{
+		Zone: q.Zone,
+	}
 
-func (q *QINIU) PutObject(local, object string, header map[string]string) (err error) {
-	// TODO: set headers
-	//putPolicy := storage.PutPolicy{
-	//	Scope: q.Bucket,
-	//}
-	//mac := qbox.NewMac(q.AccessKey, q.SecretKey)
-	//upToken := putPolicy.UploadToken(mac)
-	//cfg := storage.Config{
-	//	UseHTTPS:      false,
-	//	UseCdnDomains: false,
-	//}
-	//formUploader := storage.NewFormUploader(&cfg)
-	//ret := storage.PutRet{}
-	//putExtra := storage.PutExtra{} // 可选配置
-	//err = formUploader.PutFile(context.Background(), &ret, upToken, object, local, &putExtra)
-	return
+	manager := storage.NewBucketManager(q.mac, cfg)
+	items, _, _, _, err = manager.ListFiles(q.Bucket, prefix, "", "", limit)
+	if err != nil {
+		return
+	}
 
-}
+	for _, item := range items {
+		files = append(files, File{
+			ModTime: storage.ParsePutTime(item.PutTime),
+			Name:    objectRel(item.Key),
+			Size:    item.Fsize,
+			IsDir:   item.Fsize == 0,
+		})
+	}
 
-func (q *QINIU) DeleteObjects(objects []string) (err error) {
-	//每个batch的操作数量不可以超过1000个，如果总数量超过1000，需要分批发送
-
-	//deleteOps := make([]string, 0, len(objects))
-	//for _, key := range objects {
-	//	deleteOps = append(deleteOps, storage.URIDelete(q.Bucket, key))
-	//}
-	//
-	//mac := qbox.NewMac(q.AccessKey, q.SecretKey)
-	//cfg := storage.Config{
-	//	UseHTTPS:      false,
-	//	UseCdnDomains: false,
-	//}
-	//
-	//// 指定空间所在的区域，如果不指定将自动探测
-	//// 如果没有特殊需求，默认不需要指定
-	////cfg.Zone=&storage.ZoneHuabei
-	//bucketManager := storage.NewBucketManager(mac, &cfg)
-	//
-	//_, err = bucketManager.Batch(deleteOps)
-
-	return
-}
-
-func (q *QINIU) GetObjectURL(object string, expire int64) (urlStr string, err error) {
-	//mac := qbox.NewMac(q.AccessKey, q.SecretKey)
-	//deadline := time.Now().Add(time.Second * time.Duration(expire)).Unix() //1小时有效期
-	//urlStr = storage.MakePrivateURL(mac, q.Domain, object, deadline)
 	return
 }
